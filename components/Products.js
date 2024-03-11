033bed93fc0c370bfbf5751605ee9ef722869d02
Krzysztof Kaluza <krzysiek.kaluza@gmail.com>
2024-03-11 11:18:59 +0100

    Add Products page and component. Query and display all products in json string

---
+++ components/Products.js
@@ -0,0 +1,31 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const QUERY_ALL_PRODUCTS = gql`
+  query ALL_PRODUCTS {
+    allProducts {
+      name
+      photo {
+        image {
+          publicUrlTransformed
+        }
+        altText
+      }
+    }
+  }
+`;
+
+const Products = () => {
+  const { data, error, loading } = useQuery(QUERY_ALL_PRODUCTS);
+  // console.log('data', data);
+  if (loading) return <p>...loading</p>;
+  if (error) return <p>{error.message}</p>;
+  return (
+    <>
+      <p>All Products </p>
+      {JSON.stringify(data)}
+    </>
+  );
+};
+
+export default Products;
